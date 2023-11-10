b3209034b1a62e70c4dd711a89c26187782fe16e
kaba64 <Kazem Bazesefidpar>
2023-11-10 15:26:29 +0100

    Some code for week 1 of the course!

---
+++ w1/assert_exmple.c
@@ -0,0 +1,15 @@
+/*Example form the course and some lines added
+ Ka. Ba.
+ November 2, 2023*/
+#define NDEBUG
+#include <assert.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void){
+
+  assert(0);
+  printf("My program works well!\n");
+  printf("%d!\n",12%12);
+  return 0;
+}
