db1a96b5013b966aed5c35996371d4544777501b
kaba64 <Kazem Bazesefidpar>
2023-11-13 17:55:41 +0100

    The smaple codes presented in week 3

---
+++ w3/sample_code_course/print_out.c
@@ -0,0 +1,12 @@
+/*The code presented in the course
+  Kazem Bzesefidpar
+  November 13, 2023*/
+
+#include <stdio.h>
+
+int main(int argc,char *argv[]){
+
+  double x = 0.123456789;
+  printf("x is %-12.5e and %e and %10.5f and %lf and %10d\n",x,x,x,x,x);
+  return 0;
+}
