6ee9b083256f803462b65e6b0630bfcf5abca6b1
kaba64 <Kazem Bazesefidpar>
2023-11-15 17:59:45 +0100

    The sample code abour double spacing week 4!

---
+++ w4/sample_code_course/double_spacing.c
@@ -0,0 +1,41 @@
+/*The presented code in week 4 of the course*/
+
+#include<stdio.h>
+#include<stdlib.h> // exit()
+
+void print_file(FILE *fptr){
+  int c;
+  rewind(fptr);      /*rewind() is used to move the file 
+		       pointer to the beginning of the file stream*/
+  while((c=getc(fptr))!=EOF)
+    putc(c,stdout);
+}
+
+void double_space(FILE *ifp, FILE *ofp){
+  int c;
+  rewind(ifp);
+  while((c=getc(ifp))!=EOF){
+    putc(c,ofp);
+    if(c=='\n')
+      putc('\n',ofp);
+  }
+}
+
+int main(int argc, char *argv[]){
+  FILE *ifp, *ofp;
+  if(argc!=3){
+    printf("Need executable inputfile outputfile\n");
+    exit(1);
+  }
+  ifp = fopen(argv[1],"r+"); // read and write file
+  ofp =	fopen(argv[2],"w+"); // read and write file
+  printf("my %s file as read s \n\n",argv[1]);
+  print_file(ifp);
+  printf("\n\n");
+  double_space(ifp,ofp);
+  print_file(ofp);
+  printf("\n\n");
+  fclose(ifp);
+  fclose(ofp);
+  return 0;
+}
